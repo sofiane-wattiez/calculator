3668fcdcf9c6ff8c2b27f91473750cdd8c8ae4e5
sofiane-wattiez <sofiane.wattiez.developpeur@gmail.com>
2021-09-17 10:51:04 +0200

    view of number ok

--- src/Calculator.js
+++ src/Calculator.js
@@ -15,7 +15,7 @@ function Calculator() {
     const [value, setValue] = useState(""); // useState c'est global
     const [calc, setCalc] = useState("");
     const [result, setResult] = useState("");
-    // const ops = ['/', '*', '+', '-', '.'];
+    const ops = ['/', '*', '+', '-', '.'];
 
     // Créer une fonction pour le résultat + 
     // une fonction pour l'affichage + une pour reset
@@ -24,36 +24,72 @@ function Calculator() {
     const handleClick = (e) => {
         // console.log(e.target.value);
         setValue(calc.concat(e.target.value));
-
     }
 
-    const
+
+
+
+    const calculate = () => {
+        // || '+' || '-' || '*' || '/'
+        if (calc === '') {
+            return;
+        }
+        const value = calc.slice(0);
+        setCalc(value);
+        setCalc(eval(calc).toString());
+
+
+
+    }
+    const updateCalc = value => {
+        if (
+            ops.includes(value) && calc === '' ||
+            ops.includes(value) && ops.includes(calc.slice(-1))
+        ) {
+            return;
+        }
+
+        setCalc(calc + value);
+        if (!ops.includes(value)) {
+            setResult(eval(calc + value).toString());
+
+        }
+    }
+    // const deleteLast = () => {
+    //     if (calc === "") {
+    //         return;
+    //     }
+    //     const value = calc.slice(0, -1);
+    //     setCalc(value);
+    // }
+
+
 
     return (
         <section className="Calculator">
 
             <TheTitle value="Calculator9000" />
 
-            <BeautifullScreen value={value} />
+            <BeautifullScreen value={result} />
 
-            <GreatOperationButton value={"/"} handleClick={handleClick} />
-            <GreatOperationButton value={"*"} handleClick={handleClick} />
-            <GreatOperationButton value={"+"} handleClick={handleClick} />
-            <GreatOperationButton value={"-"} handleClick={handleClick} />
-            <GreatOperationButton value={"C"} handleClick={handleClick} />
+            <GreatOperationButton value={"/"} handleClick={updateCalc} />
+            <GreatOperationButton value={"*"} handleClick={updateCalc} />
+            <GreatOperationButton value={"+"} handleClick={updateCalc} />
+            <GreatOperationButton value={"-"} handleClick={updateCalc} />
+            <GreatOperationButton value={"C"} handleClick={updateCalc} />
             <div className="digits">
-                <AmazingNumberButton value={"0"} handleClick={handleClick} />
-                <AmazingNumberButton value={"1"} handleClick={handleClick} />
-                <AmazingNumberButton value={"2"} handleClick={handleClick} />
-                <AmazingNumberButton value={"3"} handleClick={handleClick} />
-                <AmazingNumberButton value={"4"} handleClick={handleClick} />
-                <AmazingNumberButton value={"5"} handleClick={handleClick} />
-                <AmazingNumberButton value={"6"} handleClick={handleClick} />
-                <AmazingNumberButton value={"7"} handleClick={handleClick} />
-                <AmazingNumberButton value={"8"} handleClick={handleClick} />
-                <AmazingNumberButton value={"9"} handleClick={handleClick} />
+                <AmazingNumberButton value={"0"} handleClick={updateCalc} />
+                <AmazingNumberButton value={"1"} handleClick={updateCalc} />
+                <AmazingNumberButton value={"2"} handleClick={updateCalc} />
+                <AmazingNumberButton value={"3"} handleClick={updateCalc} />
+                <AmazingNumberButton value={"4"} handleClick={updateCalc} />
+                <AmazingNumberButton value={"5"} handleClick={updateCalc} />
+                <AmazingNumberButton value={"6"} handleClick={updateCalc} />
+                <AmazingNumberButton value={"7"} handleClick={updateCalc} />
+                <AmazingNumberButton value={"8"} handleClick={updateCalc} />
+                <AmazingNumberButton value={"9"} handleClick={updateCalc} />
 
-                <MagnificientEqualButton value={"="} handleClick={handleClick} />
+                <MagnificientEqualButton value={"="} handleClick={calculate} />
             </div>
 
 
--- src/component/BeautifullScreen.js
+++ src/component/BeautifullScreen.js
@@ -3,16 +3,19 @@ import React, { useState, useEffect } from 'react';
 
 function BeautifullScreen(props) {
     // const [result, setResult] = useState(''); // useState c'est global
-    // const [calc, setCalc] = useState(''); // useState c'est global
-    // const [screenView, setScreenView] = useState('')
+    const [calc, setCalc] = useState(''); // useState c'est global
+    const [value, setValue] = useState('')
 
 
 
 
 
     return (
-        <div className="display">
+        <div className="BeatifullScreen">
             <h2 value={props.value}>{props.value}</h2>
+            {/* {value ? <span>({value})</span> : ''}
+            &nbsp;
+            {calc || "0"} */}
         </div>
     );
 
